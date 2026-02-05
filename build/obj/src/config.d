53c373edf1c0f63516ec8ee3f63859ac87e7606e
Daniel Collins <daniel.collins@4cgroup.co.za>
2026-02-05 12:12:01 +0200

    Stop tracking build artifacts and local VSCode files

--- build/obj/src/config.d
+++
@@ -1,4 +0,0 @@
-build/obj/src/config.o: src/config.cpp include/fast_engine/config.hpp \
- external/chess.hpp
-include/fast_engine/config.hpp:
-external/chess.hpp:
--- build/obj/src/transposition.d
+++
@@ -1,6 +0,0 @@
-build/obj/src/transposition.o: src/transposition.cpp \
- include/fast_engine/transposition.hpp external/chess.hpp \
- include/fast_engine/types.hpp
-include/fast_engine/transposition.hpp:
-external/chess.hpp:
-include/fast_engine/types.hpp:
